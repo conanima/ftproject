59c2ff489bb22d5f5ffd2c7d007231d02c900e87
hong <hong@PC502_19>
2017-04-26 15:45:46 +0900

    4-26

---
+++ foodTest/src/foodTest/Test01.java
@@ -0,0 +1,8 @@
+package foodTest;
+
+public class Test01 {
+	public static void main(String[] arg){
+		System.out.println("테스트입니다.");
+		
+	}
+}
---
+++ foodTest/src/foodTest/Test02.java
@@ -0,0 +1,9 @@
+package foodTest;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		System.out.println("두번째 테스트");
+	}
+
+}
